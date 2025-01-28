6ea175ee4cd1273591958452715ce8d14c418d50
lykes <felix.delasheras.upm.es@gmail.com>
2025-01-28 16:17:54 +0300

    Add Langchain tracing and update configuration

--- src/config.py
+++ src/config.py
@@ -4,6 +4,48 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+description = """
+CryptoAgent API helps you do awesome stuff. 🚀
+
+### How to use the API:
+
+1. **Creating a user and chat:**
+   - Send a POST request to `/api/user/chats/` with `wallet_id` parameters and JSON body containing `chat_uuid`, `question`, and `answer`. This will create a new chat and save it in the database.
+
+2. **Getting the list of chats:**
+   - Use a GET request to `/api/user/chats/` to get a list of all chats associated with your `wallet_id`.
+
+3. **Getting chat history:**
+   - Use a GET request to `/api/user/chats/{chat_uuid}/history` to get chat history in the format:
+     ```json
+     [
+       {
+         "question": "string",
+         "answer": "string",
+         "timestamp": "2025-01-26T18:47:58.953Z"
+       }
+     ]
+     ```
+
+### Example JavaScript for streaming text:
+```javascript
+async function streamText(url) {
+    const response = await fetch(url);
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    let result = "";
+
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        result += decoder.decode(value, { stream: true });
+        console.log(result); // print text to console
+    }
+}
+```
+
+"""
+
 
 class Settings(BaseSettings):
     STREAMING: bool = True
@@ -12,7 +54,7 @@ class Settings(BaseSettings):
     PROMPT: str = "hwchase17/openai-tools-agent"
 
     PROJECT_NAME: str = "Web Search Agent"
-    PROJECT_DESC: str = "Агент с web поиском"
+    PROJECT_DESC: str = description
     DEBUG_LOGS: bool = False
 
 
